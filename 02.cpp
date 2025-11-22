a0ce10cad453e60290a8c82777890b0a74d0a1ba
shwetha2235 <shwetha6625@gmail.com>
2025-11-22 19:34:45 +0530

    Update 02.cpp

--- 02.cpp
+++ 02.cpp
@@ -15,20 +15,4 @@ int factorial(int n)  {
 
            return 0;
  }
-#include<iostream>
-using namespace std; 
 
-int factorial(int n)  {
-        if (n == 0)
-                return 1;           // Base case 
-        return n * factorial(n- 1); // Recursive step 
-}
-
- int main() {
-          int n; cout << "Enter a number: ";
-          cin >> n;
-          int result = factorial(n);
-          cout << "Factorial of " << n << " = " << result << endl;
-
-           return 0;
- }
